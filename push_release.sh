a15c1c74616969131e558c9c87adfb47363b9e19
kernel32dev <kernel32dev@gmail.com>
2023-04-30 09:49:03 -0300

    updated push_release.sh

--- push_release.sh
+++ push_release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cargo build --release &&
-cp target/release/mc-manager.exe release/mc-manager.exe
-git add release/mc-manager.exe
-git cm "compiled release"
-git push
+cp target/release/mc-manager.exe release/mc-manager.exe &&
+git add release/mc-manager.exe &&
+git cm "compiled release" &&
+git push &&
+echo "pushed release successfully"
