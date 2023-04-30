4da301921a12726be4be07cd046eb668e8fb2413
kernel32dev <kernel32dev@gmail.com>
2023-04-30 20:24:16 -0300

    updated script message

--- scripts/mc-install.bat
+++ scripts/mc-install.bat
@@ -1,7 +1,6 @@
-@echo ""
-@echo "-----------------------------------------------------"
-@echo "Esse script tem que ser executado como administrador!"
-@echo "-----------------------------------------------------"
+@echo -----------------------------------------------------
+@echo Esse script tem que ser executado como administrador!
+@echo -----------------------------------------------------
 curl https://raw.githubusercontent.com/kernel32dev/mc-manager/master/release/mc-manager.exe -o mc-manager.exe
 curl https://raw.githubusercontent.com/kernel32dev/mc-manager/master/scripts/mc-update.bat -o mc-update.bat
 mc-manager.exe install
